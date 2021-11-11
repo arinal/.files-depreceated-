a22baa5d1dc9d7c177de5dadb21cf1359c4515a1
arinal <dicky.arinal@gmail.com>
2021-11-11 21:56:34 +0000

    Update stuff and zprezto

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -6,158 +6,150 @@ vim.opt.relativenumber = true
 vim.opt.wrap = true
 vim.opt.undofile = false
 vim.opt.showmode = false
+vim.opt_global.shortmess:remove("F")
 
--- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 lvim.builtin.which_key.mappings["sH"] = { "<cmd>Telescope command_history<cr>", "Command history" }
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = false
 lvim.builtin.terminal.active = false
-lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.bufferline.active = true
 lvim.builtin.dap.active = true
 
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "css",
-  "rust",
-  "go",
-  -- "scala",
-  "java",
-  "yaml",
-  "nix"
-}
+-- lvim.builtin.lualine.options.theme = "horizontal"
+require("user.lualine").config()
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.autotag.enable = true
+lvim.builtin.treesitter.playground.enable = false
+lvim.builtin.treesitter.rainbow.extended_mode = false
+lvim.builtin.treesitter.highlight.enabled = false
+lvim.builtin.treesitter.rainbow = {
+	enable = true,
+	colors = {
+		"Gold",
+		"Orchid",
+		"DodgerBlue",
+	},
+	-- disable = { "html" },
+}
 
 -- LSP
-lvim.lang.python.formatters = { { exe = "black", } }
+lvim.lang.emmet.active = true
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ exe = "black", filetypes = { "python" } },
+	{ exe = "stylua", filetypes = { "lua" } },
+	{ exe = "prettier", filetypes = { "markdown" } },
+})
+
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ exe = "flake8" },
+})
 
 lvim.plugins = {
-  -- themes
-  "folke/tokyonight.nvim",
-  {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}},
-  "CantoroMC/ayu-nvim",
-  "FrenzyExists/aquarium-vim",
-  "Mangeshrex/uwu.vim",
+	-- themes
+	"lunarvim/colorschemes",
+	"folke/tokyonight.nvim",
+	{ "ellisonleao/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } },
+	"CantoroMC/ayu-nvim",
+	"FrenzyExists/aquarium-vim",
+	"Mangeshrex/uwu.vim",
 
-  -- "windwp/nvim-autopairs",
-  {
-    "norcalli/nvim-colorizer.lua",
-      config = function()
-        require("colorizer").setup({ "*" }, {
-            RGB = true,
-            RRGGBB = true,
-            RRGGBBAA = true,
-            rgb_fn = true,
-            hsl_fn = true,
-            css = true,
-            css_fn = true,
-            })
-    end,
-  },
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
-  "tpope/vim-repeat",
-  { "tpope/vim-surround", keys = {"c", "d", "y"} },
-  { "ggandor/lightspeed.nvim", event = "BufRead" },
-  "github/copilot.vim",
-  "lukas-reineke/indent-blankline.nvim",
-  -- {'tzachar/compe-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-compe'},
-  -- {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true,
+				RRGGBB = true,
+				RRGGBBAA = true,
+				rgb_fn = true,
+				hsl_fn = true,
+				css = true,
+				css_fn = true,
+			})
+		end,
+	},
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	"tpope/vim-repeat",
+	"ChristianChiarulli/nvim-ts-rainbow",
+	{ "tpope/vim-surround", keys = { "c", "d", "y" } },
+	"unblevable/quick-scope",
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("user.neoscroll").config()
+		end,
+	},
+	{ "ggandor/lightspeed.nvim", event = "BufRead" },
+	{
+		"sindrets/diffview.nvim",
+		event = "BufRead",
+	},
+	"christoomey/vim-tmux-navigator",
 	"oberblastmeister/neuron.nvim",
-  "christoomey/vim-tmux-navigator",
-  "untitled-ai/jupyter_ascending.vim",
-  { "folke/trouble.nvim", cmd = "TroubleToggle" },
-  { "ray-x/lsp_signature.nvim",
-    event = "InsertEnter",
-    config = function()
-      require "lsp_signature".setup()
-    end
-  },
-  "rcarriga/nvim-dap-ui",
-  "theHamsta/nvim-dap-virtual-text",
-  -- LSP clients
-  -- {'scalameta/nvim-metals'}, requires = { "metals" },
-  {
-    "scalameta/nvim-metals",
-    config = function()
-      local metals_config = require("metals").bare_config()
-      local function map(mode, lhs, rhs, opts)
-        local options = { noremap = true }
-        if opts then
-          options = vim.tbl_extend("force", options, opts)
-        end
-        vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-      end
-      map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-      map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-      map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-      map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
--- map("n", "<leader>ln", "<cmd>lua vim.lsp.buf.rename()<CR>")
--- map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
--- map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
--- map("n", "<leader>ws", '<cmd>lua require"metals".worksheet_hover()<CR>')
--- map("n", "<leader>a", '<cmd>lua require"metals".open_all_diagnostics()<CR>')
--- map("n", "<leader>d", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>") -- buffer diagnostics only
--- map("n", "[c", "<cmd>lua vim.lsp.diagnostic.goto_prev { wrap = false }<CR>")
--- map("n", "]c", "<cmd>lua vim.lsp.diagnostic.goto_next { wrap = false }<CR>")
-      metals_config.on_attach = function()
-        require("lvim.lsp").common_on_attach()
-      end
-      metals_config.settings = {
-        showImplicitArguments = false,
-        showInferredType = true,
-        excludedPackages = {},
-      }
-      metals_config.init_options.statusBarProvider = "on"
-      -- require("metals").initialize_or_attach { metals_config }
-      vim.cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach { metals_config }]])
-    end,
-  },
-}
--- local metals_config = require("metals").bare_config()
--- metals_config.settings = {
---   showImplicitArguments = true,
---   excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
--- }
--- metals_config.on_attach = function()
---   require("lvim.lsp").common_on_attach()
--- end
--- vim.cmd([[autocmd FileType scala setlocal omnifunc=v:lua.vim.lsp.omnifunc]])
 
-vim.cmd [[
+	-- "github/copilot.vim",
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		-- event = "BufReadPre",
+		config = function()
+			require("user.blankline")
+		end,
+	},
+	-- {
+	--   "tzachar/cmp-tabnine",
+	--   config = function()
+	--     local tabnine = require "cmp_tabnine.config"
+	--     tabnine:setup {
+	--       max_lines = 1000,
+	--       max_num_results = 20,
+	--       sort = true,
+	--     }
+	--   end,
+	--   run = "./install.sh",
+	--   requires = "hrsh7th/nvim-cmp",
+	-- },
+	"untitled-ai/jupyter_ascending.vim",
+	-- { "folke/trouble.nvim", cmd = "TroubleToggle" },
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
+	"rcarriga/nvim-dap-ui",
+	"theHamsta/nvim-dap-virtual-text",
+	-- LSP clients
+	{
+		"scalameta/nvim-metals",
+		config = function()
+			require("user.scala").config()
+		end,
+	},
+}
+
+vim.cmd([[
   let g:github_enterprise_urls = ['https://github.bamtech.co']
-]]
+]])
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-}
+require("neuron").setup({
+	virtual_titles = true,
+	mappings = true,
+	run = nil,
+	neuron_dir = "~/neuron",
+	leader = "gz",
+})
 
-require'neuron'.setup {
-    virtual_titles = true,
-    mappings = true,
-    run = nil,
-    neuron_dir = "~/neuron",
-    leader = "gz",
-}
+-- local dap_install = require("dap-install")
+-- local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
+-- for _, debugger in ipairs(dbg_list) do
+-- 	dap_install.config(debugger)
+-- end
 
-local dap_install = require("dap-install")
-local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
-for _, debugger in ipairs(dbg_list) do
-	dap_install.config(debugger)
-end
-
-require("dapui").setup()
-require("nvim-dap-virtual-text").setup()
+-- require("dapui").setup()
+-- require("nvim-dap-virtual-text").setup()
